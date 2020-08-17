7adbce52819dfd5ed20ea3d37d90f0c2a7262626
D <deepak.poosa95@gmail.com>
2020-08-17 14:30:07 +0530

    icons added and ui changes

--- automateio1/src/app/app.component.ts
+++ automateio1/src/app/app.component.ts
@@ -9,4 +9,25 @@ import { NoteDataServiceService } from '../app/services/note-data-service.servic
 })
 export class AppComponent {
   title = 'automateio1';
+
+  notesDATA: any = [{
+    title: 'note1',
+    description: 'note1 description',
+    time: new Date()
+  },
+  {
+    title: 'note2',
+    description: 'note1 description2',
+    time: new Date()
+  },
+  {
+    title: 'note3',
+    description: 'note3 description',
+    time: new Date()
+  }];
+
+  constructor() {
+    localStorage.setItem('noteDB', JSON.stringify(this.notesDATA));
+  }
+
 }
